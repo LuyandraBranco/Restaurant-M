64cee0bcc341a843766041f1751cedba9668f29a
Luyandra_Branco <94245234+LuyandraBranco@users.noreply.github.com>
2024-04-12 14:24:05 +0100

    fix: warnings retirados

--- src/components/CardBlog/styles.js
+++ src/components/CardBlog/styles.js
@@ -1,10 +1,4 @@
 import styled from "styled-components";
-import {
-    hover
-} from "@testing-library/user-event/dist/hover";
-import {
-    AnimationEvent
-} from "react";
 
 export const Card = styled.div `
 
